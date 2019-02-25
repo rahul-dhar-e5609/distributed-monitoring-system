476b8be0d245003f578e4b35cd01e3e3d318c0b3
IAmRDhar <rahul.dhar47@gmail.com>
2019-02-25 23:34:57 +0530

    added gulp js for automating go install

---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "mypackage"
+
+func main() {
+	println(mypackage.PublicVar)
+	mypackage.PublicFunc()
+
+}
---
+++ src/mypackage/source.go
@@ -0,0 +1,12 @@
+package mypackage
+
+var packageVar int = 42
+var PublicVar string = "public"
+
+const (
+	FirstConst = iota
+)
+
+func PublicFunc() {
+	println("noop")
+}

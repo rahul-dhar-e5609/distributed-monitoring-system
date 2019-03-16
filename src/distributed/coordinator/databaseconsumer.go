9212a07002395f66a9953f304265225fdf40bcf1
IAmRDhar <rahul.dhar47@gmail.com>
2019-03-16 20:51:55 +0530

    Code Documentation for DatabaseConsumer methods

--- src/distributed/coordinator/databaseconsumer.go
+++ src/distributed/coordinator/databaseconsumer.go
@@ -42,30 +42,64 @@ func NewDataConsumer(er EventRaiser) *DatabaseConsumer {
 	dc.queue = qutils.GetQueue(qutils.PersistReadingsQueue,
 		dc.ch, false)
 
+	// Added listener for the event that gets published
+	// whenever a new data spurce is discovered
 	dc.er.AddListener("DataSourceDiscoveredEvents", func(eventData interface{}) {
+		// Subscribing to the newly discovered
+		// data source
 		dc.SubscribeToDataEvent(eventData.(string))
 	})
 
 	return &dc
 }
 
-// SubscribeToDataEvent
+// SubscribeToDataEvent func is reponsible for subscribing
+// to the events published.
 func (dc *DatabaseConsumer) SubscribeToDataEvent(eventName string) {
+
+	// checking if event has been subscriber to already
 	for _, v := range dc.sources {
+		//bail, if already subscribed
 		if v == eventName {
 			return
 		}
 	}
 
-	// registering new data source
+	// Subscribing new data source
+	// and listening for messages received
+	// event.
+	//
+	// Using closure for returning callback
+	// Gives a new isolated variable scope
+	// that gets created every time the func
+	// is called
+	//
+	// This allows the event handlers to register
+	// a state that is needed to throttle down the
+	// rate with which the messages are coming in
+	// from the event sources.
 	dc.er.AddListener("MessageReceived_"+eventName, func() func(interface{}) {
+
+		// state for persisting data
+		//
+		// retains their state from call to call
+		// since theire state is captured by the closure
+		// not the callback
 		prevTime := time.Unix(0, 0)
 
 		buf := new(bytes.Buffer)
 
 		return func(eventData interface{}) {
+
+			// casting event data object in
+			// EventData struct
 			ed := eventData.(EventData)
+
+			// throttle the messages to be persisted
+			// no faster than 5 seconds (maxrate)
 			if time.Since(prevTime) > maxRate {
+
+				// resetting the clock
 				prevTime = time.Now()
 
 				sm := dto.SensorMessage{
@@ -74,6 +108,10 @@ func (dc *DatabaseConsumer) SubscribeToDataEvent(eventName string) {
 					Timestamp: ed.Timestamp,
 				}
 
+				// resetting buffer to make sure there is
+				// no data from a previous call
+				//
+				// resetting state
 				buf.Reset()
 
 				enc := gob.NewEncoder(buf)
@@ -83,6 +121,8 @@ func (dc *DatabaseConsumer) SubscribeToDataEvent(eventName string) {
 					Body: buf.Bytes(),
 				}
 
+				// Publishing to persist reading queue
+				// to persist data in the database.
 				dc.ch.Publish(
 					"",
 					qutils.PersistReadingsQueue,
